a2b56fa58764cdd0e980247853e2149eade59627
neelxie <kidricederek@gmail.com>
2021-12-22 02:27:52 +0300

    picking the params array

--- pages/docs/[...params].js
+++ pages/docs/[...params].js
@@ -1,13 +1,18 @@
-import React from 'react'
-import { useRouter } from 'next/router'
+import React from "react";
+import { useRouter } from "next/router";
 
 export default function DocsPage() {
-  const router = useRouter()
-  const { params } = router.query
-  console.log(params)
+  const router = useRouter();
+  const { params = []} = router.query;
+  console.log(params);
+  if (params.length === 2) {
+    return <h1>Viewing docs for{params[0]} and concept {params[1]}</h1>;
+  } else if(params.length === 1) {
+    return <h1>Viewing docs for{params[0]}</h1>;
+  }
   return (
     <div>
       <h1>Docs home page</h1>
     </div>
-  )
+  );
 }
