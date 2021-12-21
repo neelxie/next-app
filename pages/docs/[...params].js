d04334779bacf49b21582cebbe576aee57fdc964
neelxie <kidricederek@gmail.com>
2021-12-22 02:16:42 +0300

    picking the params array

--- pages/docs/[...params].js
+++ pages/docs/[...params].js
@@ -1,6 +1,10 @@
 import React from 'react'
+import { useRouter } from 'next/router'
 
 export default function DocsPage() {
+  const router = useRouter()
+  const { params } = router.query
+  console.log(params)
   return (
     <div>
       <h1>Docs home page</h1>
