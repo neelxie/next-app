4d908cee73e0716c0fd82f6be5a68461169abaeb
neelxie <kidricederek@gmail.com>
2021-12-22 03:28:16 +0300

    Link replace prop doesnt add new url to stack

--- pages/product/index.js
+++ pages/product/index.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import Link from 'next/link'
 
-export default function Product() {
+export default function Product({productId = 23}) {
   return (
     <div><Link href="/"><a>Home</a></Link>
       <h1>All the products we have.
         
       </h1>
+      <Link href={`/product/${productId}`}><a>Product {productId}</a></Link>
     </div>
   )
 }
