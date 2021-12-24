55893466c11dde943567ae62c8c5dc564c103394
neelxie <kidricederek@gmail.com>
2021-12-24 11:19:51 +0300

    Add a components folder and add user component

--- pages/users.js
+++ pages/users.js
@@ -1,19 +1,27 @@
-import React from 'react'
+import React from "react";
+import User from "../components/user";
 
-export default function UserList({users}) {
-  return <><h1>List of Users</h1>{
-    users.map(user => <li key={user.id}>{user.name}</li>)
-    
-  }</>
+export default function UserList({ users }) {
+  return (
+    <>
+      <h1>List of Users</h1>
+      {users.map((user) => {
+        return (
+          <div key={user.id}>
+            <User user={user} />
+          </div>
+        );
+      })}
+    </>
+  );
 }
 
 export async function getStaticProps() {
-  const res = await fetch('https://jsonplaceholder.typicode.com/users')
-  const users = await res.json()
+  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  const users = await res.json();
   return {
     props: {
-      users
-    }
-  }
+      users,
+    },
+  };
 }
-
