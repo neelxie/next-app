0fa2f122c75698159c9dc616cf7f3bbd739c69b9
neelxie <kidricederek@gmail.com>
2021-12-24 17:50:47 +0300

    Add getStaticPaths for posts

--- pages/posts/[postId].js
+++ pages/posts/[postId].js
@@ -8,6 +8,17 @@ export default function Post({post}) {
   )
 }
 
+export async function getStaticPaths() {
+  return {
+    paths: [
+      { params: { postId: '1' } },
+      { params: { postId: '2' } },
+      { params: { postId: '3' } },
+    ],
+    fallback: false,
+  }
+}
+
 export async function getStaticProps(context) {
   const { params } = context
   const response = await fetch(`https://jsonplaceholder.typicode.com/posts/${params.postId}`)
