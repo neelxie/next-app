f18f62cee9e15115c160802d2b384c07c7865981
neelxie <kidricederek@gmail.com>
2022-01-03 20:13:45 +0300

    Fix error arising from hhtps

--- pages/news/index.js
+++ pages/news/index.js
@@ -1,16 +1,29 @@
-import React from 'react'
+import React from "react";
 
-function News() {
+function News({news}) {
   return (
-    <h1>
-     List of news items 
-    </h1>
-  )
+    <>
+      <h1>List of news items</h1>
+      {
+        news.map(item => (
+          <div key={item.id}>
+            <h2> {item.id} {item.title} | {item.category}</h2>
+          </div>
+        ))
+      }
+    </>
+  );
 }
 
-export default News
+export default News;
 
-export async function getServerSideProps(){
-  const response = await fetch('https://localhost:4000/news')
-  const data = await response.json()
+export async function getServerSideProps() {
+  const response = await fetch("http://localhost:4000/news");
+  const data = await response.json();
+
+  return {
+    props: {
+      news: data,
+    },
+  };
 }
